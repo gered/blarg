04f904d7f945bc227b8ec4b906f6add31d657d3a
gered <gered@blarg.ca>
2013-05-21 22:42:34 -0400

    add stub route for listing files

--- src/blarg/routes/files.clj
+++ src/blarg/routes/files.clj
@@ -1,10 +1,15 @@
 (ns blarg.routes.files
   (:use [compojure.core]
+        [noir.util.route]
         [blarg.routes.helpers]
         [blarg.util])
   (:require [blarg.views.layout :as layout]
             [blarg.models.files :as files]
             [noir.response :as resp]))
+
+(defn list-files [path]
+  (layout/render
+    "files/list.html" {:files (files/list-files path)}))
 
 (defn get-file [path]
   (if-let [file (files/get-file path)]
@@ -12,4 +17,6 @@
     (resp/status 404 nil)))
 
 (defroutes files-routes
+  (restricted GET "/listfiles" [] (list-files "/"))
+  (restricted GET "/listfiles/*" [*] (list-files (ensure-prefix * "/")))
   (GET "/files/*" [*] (get-file (ensure-prefix * "/"))))
