f6868031b52fdd7ff78bd7dc91a2dae106fce6b1
gered <gered@blarg.ca>
2013-05-21 22:42:19 -0400

    little bit of cleanup

--- src/blarg/routes/files.clj
+++ src/blarg/routes/files.clj
@@ -7,9 +7,9 @@
             [noir.response :as resp]))
 
 (defn get-file [path]
-  (if-let [file (files/get-file (ensure-prefix path "/"))]
+  (if-let [file (files/get-file path)]
     (resp/content-type (:content_type file) (:data file))
     (resp/status 404 nil)))
 
 (defroutes files-routes
-  (GET "/files/*" [*] (get-file *)))
+  (GET "/files/*" [*] (get-file (ensure-prefix * "/"))))
