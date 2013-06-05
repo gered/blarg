d7e6adcfa7ff0ea8091a8d5c3556c38e3f4ea552
gered <gered@blarg.ca>
2013-06-05 19:41:25 -0400

    add projects page and link (empty for now)

--- src/blarg/routes/home.clj
+++ src/blarg/routes/home.clj
@@ -9,6 +9,11 @@
   (layout/render
     "about.html" {:html-title (->html-title ["About"])}))
 
+(defn projects-page []
+  (layout/render
+    "projects.html" {:html-title (->html-title ["Projects"])}))
+
 (defroutes home-routes
   (GET "/about" [] (about-page))
+  (GET "/projects" [] (projects-page))
   (GET "/toslug" [text] (resp/json {:slug (if text (->slug text))})))
