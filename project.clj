8062b2e4330c21cba62bc96602da922eaed2253d
gered <gered@blarg.ca>
2014-03-23 15:52:59 -0400

    update dependency versions

--- project.clj
+++ project.clj
@@ -2,21 +2,22 @@
   :description "webapp for http://blarg.ca/"
   :url "http://blarg.ca/"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lib-noir "0.6.0"]
-                 [compojure "1.1.5"]
-                 [ring-server "0.2.8"]
+                 [lib-noir "0.8.1"]
+                 [compojure "1.1.6"]
+                 [ring-server "0.3.1"]
                  [selmer "0.2.4"]
-                 [com.taoensso/timbre "1.6.0"]
-                 [com.postspectacular/rotor "0.1.0"]
-                 [com.taoensso/tower "1.5.1"]
+                 [com.taoensso/timbre "3.0.0"]
+                 [com.taoensso/tower "2.0.1"]
+                 [environ "0.4.0"]
                  [markdown-clj "0.9.26"]
                  [com.ashafa/clutch "0.4.0-RC1"]
                  [slugger "1.0.1"]
-                 [clj-time "0.5.0"]
-                 [org.clojure/math.numeric-tower "0.0.2"]
-                 [cheshire "5.1.2"]
+                 [clj-time "0.6.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [cheshire "5.3.1"]
                  [clj-rss "0.1.3"]]
-  :plugins [[lein-ring "0.8.5"]]
+  :plugins [[lein-ring "0.8.10"]
+            [lein-environ "0.4.0"]]
   :ring {:handler blarg.handler/war-handler
          :init    blarg.handler/init
          :destroy blarg.handler/destroy}
@@ -24,6 +25,6 @@
   {:production {:ring {:open-browser? false
                        :stacktraces?  false
                        :auto-reload?  false}}
-   :dev {:dependencies [[ring-mock "0.1.3"]
-                        [ring/ring-devel "1.1.8"]]}}
+   :dev {:dependencies [[ring-mock "0.1.5"]
+                        [ring/ring-devel "1.2.1"]]}}
   :min-lein-version "2.0.0")
