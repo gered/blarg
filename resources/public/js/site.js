b56b43533d6c59ce0dc08a962259f3558f8f2229
gered <gered@blarg.ca>
2014-03-29 11:13:37 -0400

    update prettify style and add clojure highlighting support

--- resources/public/js/site.js
+++ resources/public/js/site.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
 	var code = $('div.content[role="main"]').find('pre');
-	code.addClass('prettyprint');
-	
+	code.each(function() {
+		var pre = $(this);
+
+		// markdown-clj sets up <pre> tags for use with another syntax highlighting lib that we're
+		// not using. so we need to convert some css classes to work with Prettify
+		pre.removeClass('brush:');
+		var languageHint = pre.attr('class'); // should be the only class left at this point
+
+		var classAttr = 'prettyprint';
+		if (languageHint && languageHint.length > 0)
+			classAttr += ' lang-' + languageHint;
+
+		pre.attr('class', classAttr);
+	});
+
 	prettyPrint();
 });
