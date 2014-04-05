295ea2e61aad2e92403b7339bd1c440a47b512b4
gered <gered@blarg.ca>
2014-04-05 00:07:13 -0400

    switch to highlight.js for client side syntax highlighting

--- resources/public/js/site.js
+++ resources/public/js/site.js
@@ -4,16 +4,22 @@ $(document).ready(function() {
 		var pre = $(this);
 
 		// markdown-clj sets up <pre> tags for use with another syntax highlighting lib that we're
-		// not using. so we need to convert some css classes to work with Prettify
+		// not using. so we need to convert some css classes to work with highlight.js
 		pre.removeClass('brush:');
 		var languageHint = pre.attr('class'); // should be the only class left at this point
 
-		var classAttr = 'prettyprint';
-		if (languageHint && languageHint.length > 0)
-			classAttr += ' lang-' + languageHint;
+		var classAttr = '';
+		if (languageHint && languageHint.length > 0) {
+			if (languageHint == 'no-highlight')
+				classAttr = 'no-highlight';
+			else
+				classAttr += ' lang-' + languageHint;
+		}
+
+		classAttr += ' hljs';
 
 		pre.attr('class', classAttr);
-	});
 
-	prettyPrint();
+		hljs.highlightBlock(this);
+	});
 });
